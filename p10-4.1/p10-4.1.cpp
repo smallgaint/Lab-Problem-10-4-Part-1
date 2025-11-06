31b883dc21f309f3181ba39a52646451989e1075
Sungrae Hong <s-hong.1@onu.edu>
2025-11-06 13:29:40 -0500

    Part 2 Completed

--- p10-4.1/p10-4.1.cpp
+++ p10-4.1/p10-4.1.cpp
@@ -2,24 +2,32 @@
 
 using namespace std;
 
+const int DIGIT_COUNT = 16;
+
+bool isNumeric(string s);
+bool isCorrectLength(string s);
 bool isCardValid(int digits[], int size);
 
 int main(void) {
-	int card_number;
-	const int DIGIT_COUNT = 8;
+	string card_number;
 	int digits[DIGIT_COUNT];
 
 	while (true) {
-		cout << "Enter 8-digit credit card # or Q to quit: ";
+		cout << "Enter 16-digit credit card # or Q to quit: ";
 		cin >> card_number;
 
-		if(cin.fail()) {
+		if (card_number == "q" || card_number == "Q") {
 			return 0;
+		} else if (!isNumeric(card_number)) {
+			cout << "Error - card number must contain only digits." << endl;
+			continue;
+		} else if (!isCorrectLength(card_number)) {
+			cout << "Error - card number must contain 16 digits." << endl;
+			continue;
 		}
 
-		for (int i = DIGIT_COUNT - 1; i >= 0; i--) {
-				digits[i] = card_number % 10;
-				card_number /= 10;
+		for (int i = 0; i < DIGIT_COUNT; i++) {
+			digits[i] = card_number[i] - '0';
 		}
 
 		if (isCardValid(digits, DIGIT_COUNT)) {
@@ -29,6 +37,7 @@ int main(void) {
 			cout << "Card is not valid." << endl;
 		}
 	}
+	cout << endl;
 }
 
 bool isCardValid(int digits[], int size) {
@@ -44,4 +53,17 @@ bool isCardValid(int digits[], int size) {
 	}
 	return (varify1 + varify2) % 10 == 0;
 
+}
+
+bool isNumeric(string s) {
+	for (char c : s) {
+		if (!isdigit(c)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool isCorrectLength(string s) {
+	return s.length() == DIGIT_COUNT;
 }
